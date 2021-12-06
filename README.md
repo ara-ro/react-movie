4f8e06569dce998d932542a7f8b73e909117e01c
riripono <r_ro0628@naver.com>
2021-12-06 19:04:12 +0900

    [feature] movie web service

---
+++ README.md
@@ -0,0 +1,14 @@
+## React Movie Homepage
+>**React**를 이용한 영화 홈페이지 제작하기 😊
+
+---
+
+## Installation
+React router dom [Link](https://v5.reactrouter.com/web/guides/quick-start)
+```
+npm i react-router-dom@5.3.0
+``` 
+
+
+[시청강의 : 노마드코더
+(ReactJS로 영화 웹 서비스 만들기)](https://nomadcoders.co/react-for-beginners/lobby, "강의페이지로 이동")
---
+++ src/App.js
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
+
+function App() {
+  return <Router>
+    <Switch>
+      <Route path="/hello">
+        <h1>Hello!</h1>
+      </Route>
+      <Route path="/movie">
+        <Detail />
+      </Route>
+      <Route path="/">
+        <Home />
+      </Route>
+    </Switch>
+  </Router>;
+}
+
+export default App;
